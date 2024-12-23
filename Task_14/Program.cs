fb2ae4acf596fe75ce0fcfb473d4429f999a0279
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:33:00 +0300

    Add project files.

---
+++ Task_14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    
+    internal class Program
+    {
+        public static string phrase;
+        //•	Задача 14: Напишите программу, которая считает количество слов в строке.
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            phrase = Console.ReadLine();
+            string[] pieces = phrase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("количество слов в строке: "+pieces.Length) ;
+            
+        }
+    }
+}
